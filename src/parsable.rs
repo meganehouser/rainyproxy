859b987f5b99fd17b212f3de9d4e4064930dc757
meganehouser <sleepy.st818@gmail.com>
2016-03-12 22:42:56 +0900

    add parseing chunked data

--- src/parsable.rs
+++ src/parsable.rs
@@ -37,6 +37,56 @@ pub fn parse_body(headers: &[httparse_orig::Header], buf: &[u8]) -> ParseStatus<
                 return ParseStatus::InProgress;
             }
         }
-        None => return ParseStatus::Complete(None),
+        None => {
+            match headers.iter().find(|&&h| h.name == "Trunsfer Encoding") {
+                Some(h) => {
+                    if h.value == b"chunked" {
+                        return parse_chunked(buf);
+                    }
+                }
+                None => {}
+            };
+        }
+    }
+    ParseStatus::Complete(None)
+}
+
+fn parse_chunked(buf: &[u8]) -> ParseStatus<Option<Vec<u8>>> {
+    let mut vec: Vec<u8> = Vec::new();
+    let mut cur_num = Vec::new();
+    let mut index = 3;
+    loop {
+        if buf.len() < index {
+            return ParseStatus::InProgress;
+        }
+
+        cur_num.push(buf[index - 2]);
+
+        if buf[index - 1] == b'\r' && buf[index] == b'\n' {
+            let num = str::FromStr::from_str(String::from_utf8(cur_num).unwrap().as_str()).unwrap();
+            cur_num = Vec::new();
+            if num == 0 {
+                return ParseStatus::Complete(Some(Vec::from(buf)));
+            }
+
+            index += num;
+        } else {
+            index += 1;
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::{parse_chunked, ParseStatus};
+
+    #[test]
+    fn chunked_test() {
+        let src = b"10\r\nabcdefghij\r\n0";
+        assert!(match parse_chunked(src as &[u8]) {
+            ParseStatus::Complete(_) => true,
+            _ => false,
+        });
     }
 }
